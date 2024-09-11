ff36b186305de4a98496219d53c98a97629cc3ba
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-09-11 15:21:48 +0530

    downloaded and imported images in TodoItems component

--- ToDo App/src/Components/TodoItems.jsx
+++ ToDo App/src/Components/TodoItems.jsx
@@ -1,7 +1,14 @@
 import './Assets/CSS/TodoItems.css'
+import tick from './Assets/tick.png'
+import not_tick from './Assets/not_tick.png'
+import cross from './Assets/cross.png'
 const TodoItems = () => {
   return (
-    <div>
+    <div className='todoitems'>
+        <div className="todoitems-container">
+            <img src="" alt="" />
+            <img src="" alt="" />
+        </div>
       
     </div>
   )
